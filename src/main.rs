f75457b5a79e00d706a139c247e7fcc68649443d
Geoffrey Beausire <g.beausire@criteo.com>
2021-03-31 13:22:09 +0200

    Add debugging config

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, vec};
 
 #[derive(Clone)]
 struct Record {
@@ -44,53 +44,47 @@ impl Index {
         Index {labelKeyIndex: HashMap::new()}
     }
 
-    fn get(&mut self, index_name: String) -> Field {
-        match (*self).labelKeyIndex.get(&index_name) {
-            Some(x) => x.clone(),
-            None => Field::new(),
-        }
+    fn search(&mut self, record: Record) {
+        // let t = record.labelPair.into_iter().map(|pair| {
+        //     (pair.val, self.labelKeyIndex.get(&pair.key))
+        // })
+        // .filter field not None // TODO
+        // .map(|(val, field)| {
+        //     field.field_map.get(val)
+        // });
+        println!("ok");
+        //.reduce(intesection)
     }
 
-    fn insert_record(&mut self, id: &usize, record: &Record) {
+    fn insert_record(&mut self, id: usize, record: &Record) {
         for pair in &record.labelPair {
-            let field = self.labelKeyIndex.get(&pair.key);
-            // TODO get val if exist append id
-            match field.get(&labelPair.val) {
-                Some(x) => [*x, id], -> //TODO transform in Vec
-                None => [id],
-            }
-            field.insert(
-                labelPair.val,
-                //id,
-            )
-            self.insert(labelPair.key, field);
+            let field = self.labelKeyIndex.entry(pair.key.clone()).or_insert(Field::new());
+            field.add_posting(pair.val.clone(), id);
         }
-    }
-
-    fn insert(&mut self, index_name: String, field: Field) {
-        (*self).labelKeyIndex.insert(index_name, field);
     }
 }
 
 #[derive(Clone)]
 struct Field {
-    field_map: HashMap<String, [usize; 32]>
+    field_map: HashMap<String, Vec<usize>>
 }
 
 impl<'a> Field {
     fn new() -> Field {
         Field {field_map: HashMap::new()}
     }
+
+    fn add_posting(&mut self, key: String, id: usize) {
+        let posting_list = self.field_map.entry(key).or_insert(Vec::new());
+        posting_list.push(id);
+    }
 }
 
 // https://doc.rust-lang.org/std/default/trait.Default.html
 
 fn main() {
-    // Print text to the console
-    println!("Hello World!");
-
-    let store = RecordStore::new();
-    let index = Index::new();
+    let mut store = RecordStore::new();
+    let mut index = Index::new();
     let record1 = Record{
         labelPair: vec![LabelPair{key: String::from("type"), val: String::from("fruit")}, 
                         LabelPair{key: String::from("color"), val: String::from("green")},
@@ -101,11 +95,16 @@ fn main() {
                         LabelPair{key: String::from("color"), val: String::from("green")},
                         LabelPair{key: String::from("name"), val: String::from("bean")}]
     };
-    let id = store.add(&record1);
-    index.insert_record(&id, &record1);
+    let mut id = store.add(&record1);
+    index.insert_record(id, &record1);
 
     id = store.add(&record2);
-    index.insert_record(&id, &record2);
+    index.insert_record(id, &record2);
+    
+    let record_search = Record{
+        labelPair: vec![LabelPair{key: String::from("color"), val: String::from("green")}]
+    };
+    index.search(record_search)
 }
 
 fn generator() {
