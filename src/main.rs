053b0f3550aa6446cf12eab085a92348ceda6533
Geoffrey Beausire <g.beausire@criteo.com>
2021-04-14 14:57:48 +0200

    Wrap storage logic in a storage module

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 use std::vec;
 
 mod record;
-mod store;
-mod index;
+mod storage;
 
 // https://doc.rust-lang.org/std/default/trait.Default.html
 
 fn main() {
-    let mut store = store::RecordStore::new();
-    let mut index = index::Index::new();
     let record1 = record::Record{
         label_pair: vec![record::LabelPair{key: String::from("type"), val: String::from("fruit")}, 
                         record::LabelPair{key: String::from("color"), val: String::from("green")},
@@ -24,13 +21,11 @@ fn main() {
                         record::LabelPair{key: String::from("color"), val: String::from("red")},
                         record::LabelPair{key: String::from("name"), val: String::from("strawberry")}]
     };
-    let mut id = store.add(&record1);
-    index.insert_record(id, &record1);
 
-    id = store.add(&record2);
-    index.insert_record(id, &record2);
-    id = store.add(&record3);
-    index.insert_record(id, &record3);
+    let mut storage = storage::StorageBackend::new();
+    storage.add(&record1);
+    storage.add(&record2);
+    storage.add(&record3);
     
     let record_search1 = record::Record{
         label_pair: vec![record::LabelPair{key: String::from("color"), val: String::from("green")}]
@@ -38,8 +33,8 @@ fn main() {
     let record_search2 = record::Record{
         label_pair: vec![record::LabelPair{key: String::from("name"), val: String::from("strawberry")}]
     };
-    let mut records: Vec<record::Record> = store.multi_get(index.search(record_search1));
+    let mut records = storage.search(record_search1);
     println!("Search 1: {:?}", records);
-    records = store.multi_get(index.search(record_search2));
+    records = storage.search(record_search2);
     println!("Search 2: {:?}", records);
 }
