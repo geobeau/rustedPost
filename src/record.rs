cb1480c17a5b8417ff2efcc5acd9f0acd7c002bb
Geoffrey Beausire <geobeausire@gmail.com>
2021-04-26 19:39:27 +0200

    Add regex search support

--- src/record.rs
+++ src/record.rs
@@ -1,5 +1,7 @@
 use serde::{Serialize, Deserialize};
 use std::{cmp::Eq};
+use std::fmt;
+use itertools::free::join;
 
 
 #[derive(Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq)]
@@ -22,14 +24,28 @@ impl LabelPair {
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct SearchQuery {
     pub search_fields: Vec<SearchField>,
 }
 
+impl fmt::Display for SearchQuery {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", join(self.search_fields.clone().into_iter().map(|f| format!("{}", f)), ", "))
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct SearchField {
     pub key: Box<str>,
     pub val: Box<str>,
     pub op: Operation,
+}
+
+impl fmt::Display for SearchField {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}{}\"{}\"", self.key, self.op, self.val)
+    }
 }
 
 impl SearchField {
@@ -50,8 +66,17 @@ impl SearchField {
     }
 }
 
-
+#[derive(Clone, Debug)]
 pub enum Operation {
     Eq,
     Re
+}
+
+impl fmt::Display for Operation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Operation::Eq => write!(f, "=="),
+            Operation::Re => write!(f, "=~"),
+        }
+    }
 }
