88664bdf6a0fad1ac8c68880c1dae4f7097290d1
Geoffrey Beausire <geobeausire@gmail.com>
2021-04-27 21:54:39 +0200

    WIP regex optimized search

--- src/record.rs
+++ src/record.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 use std::{cmp::Eq};
 use std::fmt;
 use itertools::free::join;
-
+use bitflags::bitflags;
 
 #[derive(Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Record {
@@ -27,11 +27,31 @@ impl LabelPair {
 #[derive(Clone, Debug)]
 pub struct SearchQuery {
     pub search_fields: Vec<SearchField>,
+    pub query_flags: QueryFlags,
+}
+
+impl SearchQuery {
+    pub fn new(search_fields: Vec<SearchField>) -> SearchQuery {
+        SearchQuery {
+            search_fields: search_fields,
+            query_flags: QueryFlags::DEFAULT
+        }
+    }
 }
 
 impl fmt::Display for SearchQuery {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", join(self.search_fields.clone().into_iter().map(|f| format!("{}", f)), ", "))
+    }
+}
+
+bitflags! {
+    pub struct QueryFlags: u8 {
+        /// Instead of doing full scans, extract a range
+        const OPTIMIZE_REGEX_SEARCH = 0b00000001;
+        /// NOT YET IMPLEMENTED: Don't perform all intersections if the result have been reduced enough
+        const ABORT_EARLY = 0b00000010;
+        const DEFAULT = Self::OPTIMIZE_REGEX_SEARCH.bits | Self::ABORT_EARLY.bits;
     }
 }
 
