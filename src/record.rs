f46d0c790daee7f5d1ea312d1f7ddde30edbacca
Geoffrey Beausire <geobeausire@gmail.com>
2021-05-30 22:41:12 +0200

    Fix wrong result when a field search is empty

--- src/record.rs
+++ src/record.rs
@@ -31,6 +31,13 @@ pub struct RCRecord {
     pub label_pairs: Vec<RCLabelPair>,
 }
 
+impl fmt::Display for RCRecord {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", join(self.label_pairs.clone().into_iter().map(|f| format!("{}", f)), ", "))
+    }
+}
+
+
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub struct RCLabelPair {
     pub key: Arc<str>,
@@ -43,6 +50,12 @@ impl RCLabelPair {
             key: Arc::from(key),
             val: Arc::from(val),
         }
+    }
+}
+
+impl fmt::Display for RCLabelPair {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}=={}", self.key, self.val)
     }
 }
 
