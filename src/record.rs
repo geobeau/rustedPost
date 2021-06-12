389b6e4917bc54f9756f0d3967682f75796cc111
Geoffrey Beausire <geobeausire@gmail.com>
2021-06-12 20:37:58 +0200

    doodling with small vecs

--- src/record.rs
+++ src/record.rs
@@ -4,6 +4,8 @@ use std::fmt;
 use itertools::free::join;
 use bitflags::bitflags;
 use std::sync::Arc;
+use std::str;
+
 
 #[derive(Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Record {
@@ -180,3 +182,76 @@ impl fmt::Display for KeyValuesSearch {
         write!(f, "{} by {}", join(self.search_fields.clone().into_iter().map(|f| format!("{}", f)), ", "), self.key_field)
     }
 }
+#[inline]
+fn next_non_space_char(chars: &[u8], start: usize) -> Option<usize> {
+    for i in start..chars.len() {
+        if chars[i] != b' ' {
+            return Some(i)
+        }
+    }
+    return None
+}
+
+#[inline]
+fn find_next(chars: &[u8], start: usize, matcher: u8) -> Option<usize> {
+    for i in start..chars.len() {
+        if chars[i] == matcher {
+            return Some(i)
+        }
+    }
+    return None
+}
+
+// TODO Add escaping support
+fn parse_record(l: &str) -> Option<Record> {
+    let chars = l.as_bytes();
+    let mut left_bound = next_non_space_char(chars, 0).unwrap();
+    if chars[left_bound] != b'{' {
+        return None
+    }
+    left_bound += 1;
+
+    let mut right_bound: usize;
+    let mut label_pairs = Vec::new();
+    loop {
+        left_bound = next_non_space_char(chars, left_bound).unwrap();
+        right_bound = find_next(chars,left_bound, b'=').unwrap();
+        let key = &chars[left_bound..right_bound];
+        left_bound = find_next(chars,left_bound, b'"').unwrap()+1;
+        right_bound = find_next(chars,left_bound, b'"').unwrap();
+        let val = &chars[left_bound..right_bound];
+        left_bound = right_bound + 1;
+        let lp = LabelPair{ key: Box::from(str::from_utf8(key).unwrap().trim_end()), val: Box::from(str::from_utf8(val).unwrap())};
+        label_pairs.push(lp);
+
+        left_bound = next_non_space_char(chars, left_bound).unwrap();
+
+        println!("{:?}", label_pairs);
+        println!("{:?}", str::from_utf8(&[chars[left_bound]]));
+        match chars[left_bound] {
+            b',' => left_bound += 1,
+            b'}' => break,
+            _ => return None,
+        };
+    } 
+    println!("{:?}", label_pairs);
+    return Some(Record{label_pairs})
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let field = "{author_family_name=\"Daniels\", author_first_name=\"B\",author_surname=\"J\", language=\"English\", year=\"0\", extension=\"rar\", title=\"Stolen Moments\",publisher=\"\",edition=\"\"}";
+        
+        parse_record(field);
+        assert_eq!(true, false);
+    }
+}
+
+
+
+
